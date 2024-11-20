073005369f4968a35d83a3eeed0a7b6aac8f42ed
Tom Sapletta <tom@sapletta.com>
2024-11-20 15:52:15 +0100

    Release version 0.1.0
    ### Added
    - Initial project setup
    - Core markdown to code conversion functionality
      - Support for multiple programming languages
      - Automatic file naming based on language
      - Directory structure creation from markdown
      - Comment-based filename detection
    - Command-line interface
    - PyPI package structure
    - Executable script installation
    
    ### Changed
    - Integrated generate.py functionality into modular package structure
    - Standardized CLI interface
    
    ### Deprecated
    - N/A
    
    ### Removed
    - N/A
    
    ### Fixed
    - N/A
    
    ### Security
    - N/A

--- setup.py
+++ setup.py
@@ -1,47 +1,7 @@
-from setuptools import setup, find_packages
+"""
+markdown2code - Convert markdown files into organized project structures with code files
+"""
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
-
-setup(
-    name="markdown2code",
-    version="0.1.0",
-    author="Plain Mark",
-    author_email="",  # Add author email
-    description="Convert markdown files into organized project structures with code files",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/plain-mark/markdown2code",
-    packages=find_packages(),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Software Development :: Code Generators",
-        "Topic :: Text Processing :: Markup :: Markdown",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-    install_requires=requirements,
-    entry_points={
-        'console_scripts': [
-            'markdown2code=markdown2code.cli:main',
-        ],
-    },
-    keywords="markdown, code generation, project structure, development tools",
-    project_urls={
-        "Bug Reports": "https://github.com/plain-mark/markdown2code/issues",
-        "Source": "https://github.com/plain-mark/markdown2code",
-    },
-)
+if __name__ == "__main__":
+    setup()
